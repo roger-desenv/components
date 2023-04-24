064c73a114c12ca6a514a8107c59876c5e516cb5
Roger Costa <roger.desenv@gmail.com>
2023-04-24 17:50:29 -0300

    adicionando modelos de components

---
+++ src/Components/CardNews.js
@@ -0,0 +1,15 @@
+
+//Hellow World
+class CardNews extends HTMLElement{
+   constructor() {
+      super()
+
+      const shadow = this.attachShadow({mode: "open"})
+      shadow.innerHTML = "<h1>Hellow World!</h1>"
+   }
+}
+
+customElements.define('card-news', CardNews)
+
+
+
